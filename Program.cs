558760c0cd2fa0b75e16bd1784fc4aa7e9f55b3d
Luis Nasser <luishnc93@gmail.com>
2021-07-11 22:39:29 -0400

    Array Exercise 3
    Remove elements

--- Program.cs
+++ Program.cs
@@ -9,7 +9,18 @@ namespace leetCodeDataStructuresAndAlgorithms
 
         static void Main(string[] args)
         {
-            
+            RemoveElement removeElement = new RemoveElement();
+
+
+            int[] nums = { 0, 1, 2, 2, 3, 0, 4, 2 }; // Input array
+            int val = 2; // Value to remove
+                         //int[] expectedNums = [...]; // The expected answer with correct length.
+                         // It is sorted with no values equaling val.
+
+            int k = removeElement.RemoveElementFunction(nums, val); // Calls your implementation
+
+            Console.WriteLine(k);
+            Console.Read();
         }
     }
 }
